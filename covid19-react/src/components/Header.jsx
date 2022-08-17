7d4c149a35cd630ce2a83863ff0e1058a048756c
DevWonny <cjfdnjs1994@naver.com>
2022-08-18 00:52:58 +0900

    feat : prettier 설정

--- covid19-react/src/components/Header.jsx
+++ covid19-react/src/components/Header.jsx
@@ -1,114 +1,120 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from 'react';
 
-import styled from "styled-components";
+import styled from 'styled-components';
 
-import {format} from "date-fns";
+import { format } from 'date-fns';
 
-import MenuModal from "./MenuModal";
+import MenuModal from './MenuModal';
 
-const Header = ()=>{
-    // 실시간 날짜 및 시간
-    const [todayDate, setTodayDate]= useState('');
-    const [realTime, setRealTime] = useState('');
+const Header = () => {
+  // 실시간 날짜 및 시간
+  const [todayDate, setTodayDate] = useState('');
+  const [realTime, setRealTime] = useState('');
 
-    // menu modal
-    const [isMenu, setIsMenu] = useState(false);
+  // menu modal
+  const [isMenu, setIsMenu] = useState(false);
 
-    useEffect(()=>{
-        setTodayDate(format(new Date(), 'yyyy.MM.dd'));
-        setInterval(()=>{
-            setRealTime(format(new Date(),'p'));
-        },1000)
-    },[])
+  useEffect(() => {
+    setTodayDate(format(new Date(), 'yyyy.MM.dd'));
+    setInterval(() => {
+      setRealTime(format(new Date(), 'p'));
+    }, 1000);
+  }, []);
 
-    return(
-        <>
-        <HeaderContainer isMenu={isMenu}>
+  return (
+    <>
+      <HeaderContainer isMenu={isMenu}>
+        <div
+          className="hamburger"
+          onClick={() => {
+            setIsMenu(!isMenu);
+          }}
+        >
+          <span className="line line-top" isMenu={isMenu}></span>
+          <span className="line line-middle" isMenu={isMenu}></span>
+          <span className="line line-bottom" isMenu={isMenu}></span>
+        </div>
 
-            <div className="hamburger" onClick={()=>{setIsMenu(!isMenu)}} >
-                <span className="line line-top" isMenu={isMenu}></span>
-                <span className="line line-middle" isMenu={isMenu}></span>
-                <span className="line line-bottom" isMenu={isMenu}></span>
-            </div>
-
-            <Title>Careful Covid</Title>
-            <DateWarp>
-                <p>{todayDate}</p>
-                <p>{realTime}</p>
-            </DateWarp>
-
-        </HeaderContainer>
-    {isMenu && <MenuModal />}
-        </>
-    )
-}
+        <Title>Careful Covid</Title>
+        <DateWarp>
+          <p>{todayDate}</p>
+          <p>{realTime}</p>
+        </DateWarp>
+      </HeaderContainer>
+      {isMenu && <MenuModal />}
+    </>
+  );
+};
 
 export default Header;
 
 const HeaderContainer = styled.div`
-  position : absolute;
-  top : 0;
-  left : 0;
-  width : 100%;
-  height : 80px;
-  background : #F8F7F9;
-  box-shadow: ${(props) => !props.isMenu && "0 4px 11px -1px rgba(9, 21, 64, 0.25)" };
-  color : #091540;
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 80px;
+  background: #f8f7f9;
+  box-shadow: ${(props) =>
+    !props.isMenu && '0 4px 11px -1px rgba(9, 21, 64, 0.25)'};
+  color: #091540;
   display: flex;
   justify-content: space-between;
-  align-items : center;
-  
+  align-items: center;
 
-  .hamburger{
-    width : 24px;
-    height : 24px;
-    margin-left : 28px;
-    position : relative;
+  .hamburger {
+    width: 24px;
+    height: 24px;
+    margin-left: 28px;
+    position: relative;
     display: flex;
     align-items: center;
     justify-content: center;
   }
-  .line{
-    position : absolute;
-    width : 24px;
-    height : 4px;
-    background-color : #000;
+
+  .line {
+    position: absolute;
+    width: 24px;
+    height: 4px;
+    background-color: #000;
     border-radius: 3px;
-    transition: transform .25s, opacity .35s;
+    transition: transform 0.25s, opacity 0.35s;
   }
-  .line-top{
-    transform : translateY(-7px);
-    transform: ${props => props.isMenu && "rotate(45deg)"};
+
+  .line-top {
+    transform: translateY(-7px);
+    transform: ${(props) => props.isMenu && 'rotate(45deg)'};
   }
-  .line-middle{
-    transform : ${props=>props.isMenu && "translateX(16px)"};
-    opacity: ${props=>props.isMenu && "0"};
+
+  .line-middle {
+    transform: ${(props) => props.isMenu && 'translateX(16px)'};
+    opacity: ${(props) => props.isMenu && '0'};
   }
+
   .line-bottom {
-    transform : translateY(7px);
-    transform: ${props => props.isMenu && "rotate(-45deg)"};
+    transform: translateY(7px);
+    transform: ${(props) => props.isMenu && 'rotate(-45deg)'};
   }
 `;
 
 const Title = styled.p`
-  font-weight : bold;
-  font-size : 24px;
-  margin-left : 28px;
+  font-weight: bold;
+  font-size: 24px;
+  margin-left: 28px;
 `;
 
 const DateWarp = styled.div`
-  width : 80px;
-  height : 30px;
-  margin-right : 6px;
-  font-size : 14px;
+  width: 80px;
+  height: 30px;
+  margin-right: 6px;
+  font-size: 14px;
   font-weight: 900;
   display: flex;
   flex-direction: column;
   justify-content: space-between;
   align-items: center;
-  & p{
-    margin : 0;
+
+  & p {
+    margin: 0;
   }
 `;
-
-
