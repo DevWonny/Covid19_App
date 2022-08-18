c160f59f14d9c4f4878b59fee0f854b0f7c7c967
DevWonny <cjfdnjs1994@naver.com>
2022-08-18 16:48:08 +0900

    feat : 메인 페이지 퍼블리싱 작업 완료

--- covid19-react/src/views/MainPage.jsx
+++ covid19-react/src/views/MainPage.jsx
@@ -1,15 +1,17 @@
 import React from "react";
 import styled from "styled-components";
 import NextIcon from "../assets/icon/NextIcon.svg";
+import GenderIcon from "../assets/icon/genderIcon.svg";
+import MapIcon from "../assets/icon/mapIcon.svg";
 
 const MainPage = () => {
   return (
     <MainContainer>
       <LiveStatus>
-        <LiveTitleDiv>
-          <LiveTitle>실시간 Covid19 현황</LiveTitle>
-          <LiveSubTitle>(2022.08.17 19:00 기준)</LiveSubTitle>
-        </LiveTitleDiv>
+        <SectionTitleDiv>
+          <SectionTitle>실시간 Covid19 현황</SectionTitle>
+          <SectionSubTitle>(2022.08.17 19:00 기준)</SectionSubTitle>
+        </SectionTitleDiv>
 
         <LiveContent>
           <LiveGraphDiv>그래프</LiveGraphDiv>
@@ -25,20 +27,38 @@ const MainPage = () => {
 
       <SmallStatusContainer>
         <SmallStatus>
-          연령별
+          <SmallIcon>
+            <img src={GenderIcon} alt="genderIcon" />
+          </SmallIcon>
+          <SmallTitle>성별 / 연령별 현황</SmallTitle>
+
           <NextIconDiv>
             <img src={NextIcon} alt="NextIcon" />
           </NextIconDiv>
         </SmallStatus>
+
         <SmallStatus>
-          지역별
+          <SmallIcon>
+            <img src={MapIcon} alt="mapIcon" />
+          </SmallIcon>
+          <SmallTitle>지역별 현황</SmallTitle>
+
           <NextIconDiv>
             <img src={NextIcon} alt="NextIcon" />
           </NextIconDiv>
         </SmallStatus>
       </SmallStatusContainer>
 
-      <DeathStatus>사망자 현황</DeathStatus>
+      <DeathStatus>
+        <SectionTitleDiv>
+          <SectionTitle>사망자 현황</SectionTitle>
+          <SectionSubTitle>(2022.08.17 19:00 기준)</SectionSubTitle>
+        </SectionTitleDiv>
+        <DeathGraphDiv>그래프</DeathGraphDiv>
+        <DeathText>일일 사망자 : 42명</DeathText>
+        <DeathText>누적 사망자 : 25,752명</DeathText>
+        <DeathText>최근 7일간 일평균 : 52명</DeathText>
+      </DeathStatus>
     </MainContainer>
   );
 };
@@ -50,7 +70,7 @@ const MainContainer = styled.div`
   top: 80px;
   left: 0;
   width: 100%;
-  height: calc(100vh - 80px);
+  overflow: hidden;
   background: #f8f7f9;
 `;
 
@@ -67,17 +87,17 @@ const LiveStatus = styled.div`
   overflow: hidden;
 `;
 
-const LiveTitleDiv = styled.div`
+const SectionTitleDiv = styled.div`
   width: 100%;
   height: 20px;
   line-height: 20px;
 `;
-const LiveTitle = styled.span`
+const SectionTitle = styled.span`
   font-weight: 900;
   font-size: 18px;
   color: #1334ab;
 `;
-const LiveSubTitle = styled.span`
+const SectionSubTitle = styled.span`
   font-size: 12px;
   color: #1334ab;
   margin-left: 5px;
@@ -124,6 +144,25 @@ const SmallStatus = styled.div`
   border-radius: 5px;
   background: #f8f7f9;
   box-sizing: border-box;
+  text-align: center;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+const SmallTitle = styled.p`
+  font-size: 18px;
+  font-weight: 900;
+  color: #1334ab;
+`;
+const SmallIcon = styled.div`
+  width: 24px;
+  height: 24px;
+  margin-right: 5px;
+
+  & img {
+    width: 100%;
+    height: 100%;
+  }
 `;
 const NextIconDiv = styled.div`
   position: absolute;
@@ -140,10 +179,23 @@ const NextIconDiv = styled.div`
 
 const DeathStatus = styled.div`
   width: 95%;
-  height: 300px;
+  height: 400px;
   box-shadow: 0 1px 6px 3px rgba(9, 21, 64, 0.25);
   border-radius: 5px;
   background: #f8f7f9;
-  margin: 30px auto 0;
+  margin: 30px auto 30px;
   box-sizing: border-box;
+  padding: 5px 0 0 5px;
+`;
+const DeathGraphDiv = styled.div`
+  width: calc(100% - 5px);
+  height: 70%;
+  background: gray;
+  margin-top: 10px;
+`;
+const DeathText = styled.p`
+  margin-top: 10px;
+  margin-bottom: 0;
+  color: #1334ab;
+  font-size: 13px;
 `;
